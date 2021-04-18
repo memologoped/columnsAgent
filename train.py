2f6d1b856f2009e4924df6a4757c76c2ab50ce79
alex-snd <alexandershulga.sh@gmail.com>
2021-04-18 22:04:07 +0300

    Add elapsed time metric

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
+import math
+from datetime import datetime
 from os import listdir
 from os.path import join
+from time import time
 
 import torch
 from torch.nn import CrossEntropyLoss
@@ -8,12 +11,16 @@ from torch.utils.data import DataLoader
 import config
 from data import WikiDataset, Collate
 from model import ZReader
-from datetime import datetime
 
 
-def epoch_training(epoch_idx: int, z_reader: ZReader, train_loader: DataLoader, optimizer, device: torch.device,
+def epoch_training(offset: int, z_reader: ZReader, train_loader: DataLoader, optimizer, scheduler, device: torch.device,
                    log_interval: int, criterion: eval, printable: bool = True) -> None:
     z_reader.train()
+
+    if printable:
+        print(f'\nLearning Rate: {round(scheduler.get_last_lr()[0], 5)}\n{"*" * 79}')
+
+    start_time = time()
 
     for batch_idx, (src, tgt_inp, tgt, src_pad_mask, tgt_pad_mask, tgt_attn_mask) in enumerate(train_loader, start=1):
         src = src.to(device)
@@ -33,17 +40,25 @@ def epoch_training(epoch_idx: int, z_reader: ZReader, train_loader: DataLoader, 
         torch.nn.utils.clip_grad_norm_(z_reader.parameters(), max_norm=0.5)
         optimizer.step()
 
-        if printable and (epoch_idx * batch_idx) % log_interval == 0:
+        if printable and (offset + batch_idx) % log_interval == 0:
             accuracy = (torch.argmax(tgt_out, dim=1) == tgt).float().sum() / tgt.size(0)
-            print(f'Train Batch: {epoch_idx * batch_idx:>7} | Loss: {loss.item():.6f} | Accuracy: {accuracy:.3f}')
+
+            print(f'Train Batch:  {offset + batch_idx:^7} | Loss: {loss.item():>10.6f} | Accuracy: {accuracy:>6.3f} | '
+                  f'Elapsed: {time() - start_time:>7.3f}s')
+
+            start_time = time()
 
 
-def epoch_testing(epoch_idx: int, z_reader: ZReader, test_loader: DataLoader, device: torch.device, criterion: eval,
+def epoch_testing(offset: int, z_reader: ZReader, test_loader: DataLoader, device: torch.device, criterion: eval,
                   printable: bool = True) -> None:
     z_reader.eval()
 
+    if printable:
+        print('-' * 79)
+
     test_loss = 0
     test_accuracy = 0
+    start_time = time()
 
     with torch.no_grad():
         for batch_idx, (src, tgt_inp, tgt, src_pad_mask, tgt_pad_mask, tgt_attn_mask) in enumerate(test_loader,
@@ -59,21 +74,24 @@ def epoch_testing(epoch_idx: int, z_reader: ZReader, test_loader: DataLoader, de
             tgt_out = tgt_out.view(-1, z_reader.token_size)
 
             loss = criterion(tgt_out, tgt).item()
-            accuracy = (torch.argmax(tgt_out, dim=1) == tgt).float().sum() / tgt.size[0]
+            accuracy = (torch.argmax(tgt_out, dim=1) == tgt).float().sum() / tgt.size(0)
 
             test_loss += loss
             test_accuracy += accuracy
 
             if printable:
-                print(f'Test Batch: {epoch_idx * batch_idx:>7} | Loss: {loss:.6f} | Accuracy: {accuracy:.3f}')
+                print(f'Test Batch:   {offset + batch_idx:^7} | Loss: {loss:>10.6f} | Accuracy: {accuracy:>6.3f} | '
+                      f'Elapsed: {time() - start_time:>7.3f}s')
+
+                start_time = time()
 
         test_loss /= batch_idx
         test_accuracy /= batch_idx
 
-        print(f'Test Average: {epoch_idx:>7} | Loss: {test_loss:.6f} | Accuracy: {test_accuracy:.3f}')
+        print(f'Test Average: {"":^7} | Loss: {test_loss:>10.6f} | Accuracy: {test_accuracy:>6.3f} |')
 
 
-def epoch_visualization(epoch_idx: int, z_reader: ZReader, vis_loader: DataLoader, device: torch.device) -> None:
+def epoch_visualization(offset: int, z_reader: ZReader, vis_loader: DataLoader, device: torch.device) -> None:
     z_reader.eval()
 
     with torch.no_grad():
@@ -93,7 +111,7 @@ def epoch_visualization(epoch_idx: int, z_reader: ZReader, vis_loader: DataLoade
 
 def save_parameters(epoch_idx: int, z_reader: ZReader) -> None:
     date = datetime.now()
-    weights_name = f'{date.month}{date.day}{date.hour}{date.minute}_{epoch_idx}'
+    weights_name = f'{date.month:0>2}{date.day:0>2}_{date.hour:0>2}{date.minute:0>2}_{epoch_idx}'
 
     z_reader.save_parameters(filename=join(config.weights_path, weights_name))
 
@@ -109,7 +127,7 @@ def train() -> None:
     train_dataset_size = 4096
     test_dataset_size = 256
     vis_dataset_size = 8
-    num_workers = 2
+    num_workers = 5
     max_noise = 8
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # --------------------------------------------MODEL PARAMETERS------------------------------------------------------
@@ -156,20 +174,22 @@ def train() -> None:
                        d_model=d_model, n_heads=n_heads, d_ff=d_ff, dropout=dropout).to(device)
 
     if pre_trained:
-        z_reader.load_parameters('path/to/weights', device=device)
+        z_reader.load_parameters(join(config.weights_path, '0418_2043_3'), device=device)
 
     optimizer = torch.optim.SGD(z_reader.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=lr_step_size, gamma=gamma)
 
     try:
         for epoch_idx in range(1, n_epochs + 1):
-            epoch_training(epoch_idx, z_reader, train_loader, optimizer, device, log_interval, criterion,
+            offset = math.ceil((epoch_idx - 1) * train_dataset_size / batch_size)
+
+            epoch_training(offset, z_reader, train_loader, optimizer, scheduler, device, log_interval, criterion,
                            printable=train_printable)
 
-            epoch_testing(epoch_idx, z_reader, test_loader, device, criterion, printable=test_printable)
+            epoch_testing(offset, z_reader, test_loader, device, criterion, printable=test_printable)
 
-            if epoch_idx % saving_interval == 0:
-                epoch_visualization(epoch_idx, z_reader, vis_loader, device)
+            if epoch_idx % vis_interval == 0:
+                epoch_visualization(offset, z_reader, vis_loader, device)
 
             if epoch_idx % saving_interval == 0:
                 save_parameters(epoch_idx, z_reader)
