78709bdb3272359b517f101fe39fba661884d8b9
alex-snd <alexandershulga.sh@gmail.com>
2021-04-21 21:50:07 +0300

    Add validation dataset path

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ from os.path import join
 
 data_path = '.\\data'
 train_path = join(data_path, 'train')
+validation_path = join(data_path, 'validation')
 test_path = join(data_path, 'test')
 weights_path = '.\\weights'
