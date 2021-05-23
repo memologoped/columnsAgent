3e84d421d0dde25aa74a7012c1bc172ac4d502d7
Vitali <network.for.social@mail.ru>
2021-05-23 16:50:53 +0300

    add fourie model

--- fnet_model.py
+++ fnet_model.py
@@ -29,6 +29,53 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
+class FeedForward(nn.Module):
+    def __init__(self, num_features, expansion_factor, dropout):
+        super().__init__()
+        num_hidden = expansion_factor * num_features
+        self.fc1 = nn.Linear(num_features, num_hidden)
+        self.fc2 = nn.Linear(num_hidden, num_features)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.dropout1(F.gelu(self.fc1(x)))
+        out = self.dropout2(self.fc2(x))
+        return out
+
+
+def fourier_transform(x):
+    return torch.fft.fft2(x, dim=(-1, -2)).real
+
+
+class FNetEncoderLayer(nn.Module):
+    def __init__(self, d_model, expansion_factor, dropout):
+        super().__init__()
+        self.ff = FeedForward(d_model, expansion_factor, dropout)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+
+    def forward(self, x):
+        residual = x
+        x = fourier_transform(x)
+        x = self.norm1(x + residual)
+        residual = x
+        x = self.ff(x)
+        out = self.norm2(x + residual)
+        return out
+
+
+class FNet(nn.TransformerEncoder):
+    def __init__(self, d_model=256, expansion_factor=4, dropout=0.5, num_layers=6):
+        encoder_layer = FNetEncoderLayer(d_model, expansion_factor, dropout)
+        super().__init__(encoder_layer=encoder_layer, num_layers=num_layers)
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
 class ZReader(nn.Module):
     def __init__(self, token_size: int, pe_max_len: int, num_layers: int, d_model: int, n_heads: int, d_ff: int,
                  dropout: float = 0.1):
@@ -40,9 +87,7 @@ class ZReader(nn.Module):
 
         self.pe = PositionalEncoding(d_model=d_model, dropout=dropout, max_len=pe_max_len)
 
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=n_heads, dim_feedforward=d_ff,
-                                                        dropout=dropout)
-        self.encoder = nn.TransformerEncoder(encoder_layer=self.encoder_layer, num_layers=num_layers)
+        self.f_net = FNet(d_model=d_model, dropout=dropout, num_layers=num_layers)
 
         self.decoder_layer = nn.TransformerDecoderLayer(d_model=d_model, nhead=n_heads, dim_feedforward=d_ff,
                                                         dropout=dropout)
@@ -69,7 +114,7 @@ class ZReader(nn.Module):
         tgt_inp = F.relu(self.mapping(tgt_inp)) * self.scale
         tgt_inp = self.pe(tgt_inp)
 
-        encoded_src = self.encoder(src=src, src_key_padding_mask=src_pad_mask)
+        encoded_src = self.f_net(src)
 
         decoded = self.decoder(tgt=tgt_inp, memory=encoded_src, tgt_mask=tgt_attn_mask,
                                tgt_key_padding_mask=tgt_pad_mask, memory_key_padding_mask=src_pad_mask)
